6a4b8f90ab2780e3e147d32339614a0cdfd648f9
J. Betro <jaredbetro@gmail.com>
2024-10-29 13:56:37 +0000

    Commit 3

---
+++ models/staging/raw/stg_raw__products.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'products') }}
+
+),
+
+renamed as (
+
+    select
+        products_id,
+        purchse_price, 
+        CAST(purchse_price AS FLOAT64) AS purchase_price, 
+         
+
+    from source
+
+)
+
+select * from renamed
